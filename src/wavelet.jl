35ebdb0ce09d59b005ad773eee2169b6a28af24f
mtaoraki <felix@felix-E330>
2015-07-05 12:55:54 +0200

    round upgrade

--- src/wavelet.jl
+++ src/wavelet.jl
@@ -1,15 +1,15 @@
 function wavelet(Y,dt,pad=0,dj=0.25,s0=2*dt,J1=-1,mother="MORLET",param=-1)
 
-Y=Y[:];	
+Y=Y[:];
 n1 = length(Y);
 
 if J1 == -1
-	 J1=round(Cint,(log(n1*dt/s0)/log(2))/dj);
+	 J1=int((log(n1*dt/s0)/log(2))/dj);
 end
 #....construct time series to analyze, pad if necessary
 x = Y - mean(Y);
 if pad == 1
-	base2 = round(Cint,log(n1)/log(2) + 0.4999);   # power of 2 nearest to N
+	base2 = int(log(n1)/log(2) + 0.4999);   # power of 2 nearest to N
 	x = [x, zeros(2^(int(base2)+1)-n1)];
 end
 n = length(x);
@@ -17,7 +17,7 @@ n = length(x);
 #....construct wavenumber array used in transform [Eqn(5)]
 k = 1:round(Cint,n/2);
 k = k*((2*pi)/(n*dt));
-k = [0.;  k;  -k[(round(Cint,(n-1)/2)-1):-1:1]];
+k = [0.;  k;  -k[(int((n-1)/2)-1):-1:1]];
 #....compute FFT of the (padded) time series
 ft=plan_fft(x);
 f = ft(x);    # [Eqn(3)]
@@ -30,7 +30,7 @@ wave = zeros(Complex,J1+1,n);  # define the wavelet array
 fourier_factor=0;
 coi=zeros(size(x));
 for a1 in 1:J1+1
-	daughter, fourier_factor, coi, dofmin=wave_bases(mother,k,scale[a1],param)	
+	daughter, fourier_factor, coi, dofmin=wave_bases(mother,k,scale[a1],param)
 	wave[a1,:] = ift(f.*daughter);  # wavelet transform[Eqn(4)]
 	#result = wave,fourier_factor, coi, dofmin;
 end
