9a1c032949e8037fa42ae809a10c86f4a23bd764
mtaoraki <felix@felix-E330>
2015-07-20 20:54:12 +0200

    speedup

--- src/wave_bases.jl
+++ src/wave_bases.jl
@@ -9,18 +9,20 @@ if mother == "MORLET"  #-----------------------------------  Morlet
 	end
 	k0 = param;
     expnt = -(scale*k - k0).^2/2;
-	for i in 1:n
-		if k[i] <= 0
-			expnt[i] = 0
-		end
-	end
+		# expnt[k.<0]=0.;
+	# for i in 1:n
+	# 	if k[i] <= 0
+	# 		expnt[i] = 0
+	# 	end
+	# end
 	norm = sqrt(scale*k[2])*(pi^(-1//4))*sqrt(n);    # total energy=N   [Eqn(7)]
 	daughter = norm*exp(expnt);
-	for i in 1:n
-		if k[i] <= 0
-			daughter[i] = 0
-		end
-	end
+	daughter[k.<0]=0.;
+	# for i in 1:n
+	# 	if k[i] <= 0
+	# 		daughter[i] = 0
+	# 	end
+	# end
 	fourier_factor = (4*pi)/(k0 + sqrt(2 + k0^2)); # Scale-->Fourier [Sec.3h]
 	coi = fourier_factor/sqrt(2);                  # Cone-of-influence [Sec.3g]
 	dofmin = 2;                                    # Degrees of freedom
@@ -30,18 +32,19 @@ elseif mother == "PAUL"  #--------------------------------  Paul
 	end
 	m = param;
 	expnt = -(scale*k);
-	for i in 1:n
-		if k[i] <= 0
-			expnt[i] = 0
-		end
-	end
+	# for i in 1:n
+	# 	if k[i] <= 0
+	# 		expnt[i] = 0
+	# 	end
+	# end
 	norm = sqrt(scale*k[2])*(2^m/sqrt(m*prod(2:(2*m-1))))*sqrt(n);
 	daughter = norm*((scale*k).^m).*exp(expnt);
-	for i in 1:n
-		if k[i] <= 0
-			daughter[i] = 0
-		end
-	end
+	# for i in 1:n
+	# 	if k[i] <= 0
+	# 		daughter[i] = 0
+	# 	end
+	# end
+	daughter[k.<0]=0.;
 	fourier_factor = 4*pi/(2*m+1);
 	coi = fourier_factor*sqrt(2);
 	dofmin = 2;
@@ -51,18 +54,19 @@ elseif mother == "DOG"  #--------------------------------  DOG
 	end
 	m = param;
 	expnt = -(scale*k).^2/2.0;
-	for i in 1:n
-		if k[i] <= 0
-			expnt[i] = 0
-		end
-	end
+	# for i in 1:n
+	# 	if k[i] <= 0
+	# 		expnt[i] = 0
+	# 	end
+	# end
 	norm = sqrt(scale*k[2]/gamma(m+0.5))*sqrt(n);
 	daughter = -norm*(i^m)*((scale*k2)^m).*exp(expnt);
-	for i in 1:n
-		if k[i] <= 0
-			daughter[i] = 0
-		end
-	end
+	# for i in 1:n
+	# 	if k[i] <= 0
+	# 		daughter[i] = 0
+	# 	end
+	# end
+	daughter[k.<0]=0.;
 	fourier_factor = 2*pi*sqrt(2./(2*m+1));
 	coi = fourier_factor/sqrt(2);
 	dofmin = 1;
